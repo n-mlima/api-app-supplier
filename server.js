4f752543ed1db3be04a98be327c731ab4b695d00
Nailson Lima <147450945+n-mlima@users.noreply.github.com>
2024-06-10 15:33:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const jsonServer = require('json-server');
+// JSON Server module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 3000;
-
 server.use(middlewares);
+// Add this before server.use(router)
 server.use(
-  // Add custom route here if needed
-  jsonServer.rewriter({
-   "/*": "/$1",
-  })
- );
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/*": "/$1",
+ })
+);
 server.use(router);
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
 });
-
 
 // Export the Server API
 module.exports = server;
